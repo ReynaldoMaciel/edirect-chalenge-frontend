36e97b5f0485638b19e6c3572c1edf5039c6b61c
Reynaldo <reynaldommaciel@gmail.com>
2019-08-05 21:57:34 -0300

    change api url

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import store from '../store'
 let authorization = store.state.token.token ? store.state.token.token : null
 
 let api = axios.create({
-  // baseURL: 'https://chalenge-edirect-backend.herokuapp.com/',
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://chalenge-edirect-backend.herokuapp.com/',
+  // baseURL: 'http://localhost:3001/',
   timeout: 5000,
   headers: { authorization }
 });
